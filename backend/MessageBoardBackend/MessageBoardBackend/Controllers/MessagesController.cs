b2c41c7be0f177b9502c4a4d25fffc35fa646501
Yang Lu (Leon) <yanglyu.leon.7@gmail.com>
2019-01-07 14:49:16 -0800

    demo: Creating a service
    module 3.2

---
+++ backend/MessageBoardBackend/MessageBoardBackend/Controllers/MessagesController.cs
@@ -0,0 +1,27 @@
+﻿using MessageBoardBackend.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MessageBoardBackend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Messages")]
+    public class MessagesController : Controller
+    {
+        public IEnumerable<Message> Get() {
+            return new Message[] {
+                new Message{
+                    Owner = "Leon",
+                    Content = "Hello"
+                },
+                new Message{
+                    Owner = "QQ",
+                    Content = "Hi",
+                },
+            };
+        }
+    }
+}
